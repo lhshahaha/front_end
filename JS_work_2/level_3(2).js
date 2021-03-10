94db40bd9d6cbc6c6b6b96605a93278cfb3e5440
lhshahaha <2392541095@qq.com>
2021-03-10 20:44:54 +0800

    第三次作业

--- JS_work_2/level_3(2).js
+++ JS_work_2/level_3(2).js
@@ -5,8 +5,8 @@ function Cat(name, color) {
   this.name = name;
   this.color = color;
 }
-var animal = new Animal();
+Cat.prototype.Species=Animal;
 //new一个猫的对象cat1也拥有了“动物”的species属性
 var cat1 = new Cat("大毛", "黄色");
-cat1.species = animal.species;
+cat1.Species();
 console.log(cat1.species); // 动物
